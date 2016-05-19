ef3ac976e8e97302a96225920d809f4f1316a08a
Ro Kleine Sonne <ro@greengoenergy.dk>
2016-05-19 15:22:28 +0200

    getting ready

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,10 +5,10 @@
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
-                <div class="panel-heading">Welcome</div>
+                <div class="panel-heading">Welcome to GreenGo Support</div>
 
                 <div class="panel-body">
-                    Your Application's Landing Page.
+                    Here you will be able to find manuals for the components installed in your PV plant by GreenGo Energy, instructional guides to their usage and answers to frequently asked questions.
                 </div>
             </div>
         </div>
