74788a5ae82382223ec28a8def60531dd08c9b01
Ro Kleine Sonne <ro@greengoenergy.dk>
2016-08-30 13:02:17 +0200

    added datahub retur

--- app/Http/Controllers/DatahubController.php
+++ app/Http/Controllers/DatahubController.php
@@ -14,7 +14,7 @@ class DatahubController extends Controller
 
     public function retur(){
 
-      $key = Input::get('thirdPartyKey', 'default key');
+      //$key = Input::get('thirdPartyKey', 'default key');
 
       //dd($key);
 
